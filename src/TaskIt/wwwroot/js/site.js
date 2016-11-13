d019afe306dd74876bdeea802832dce967d8dbe1
mcinnes01 <mcinnes01@hotmail.com>
2016-11-13 21:11:07 +0000

    added missing frontend dependencies

--- src/TaskIt/wwwroot/js/site.js
+++ src/TaskIt/wwwroot/js/site.js
@@ -1,1 +1,41 @@
-﻿// Write your Javascript code.
+﻿$(function () {
+    $(document).on('click', '.renderget', function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        var $detailsDiv = $('#' + $(this).data('target')),
+            url = $(this).data('actionurl');
+
+        $.get(url, function (data) {
+            $detailsDiv.replaceWith(data);
+        });
+    });
+});
+
+$(function () {
+    $(document).on('click', '.renderpost', function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        var $detailsDiv = $('#' + $(this).data('target')),
+            url = $(this).data('actionurl');
+
+        $.post(url, function (data) {
+            $detailsDiv.replaceWith(data);
+        });
+    });
+});
+
+$(function () {
+    $(document).on('click', '.colour-btn', function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        $('#cp1').colorpicker();
+    })
+});
+
+$(function () {
+    var elements = document.querySelectorAll('.editable'),
+    editor = new MediumEditor(elements);
+});
